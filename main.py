18b74799ba523cce792fa2cbb1706c87feec5fae
Rhiannon Orizaga <orizaga.jones@gmail.com>
2017-07-11 20:52:13 -0700

    all changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = '1234567890'
 
-
+#create a new class that can be managed by sqlalchemy & store blogs in a db
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
@@ -16,11 +16,13 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, owner): #name instead of title?
+    def __init__(self, title, body, owner): #name instead of title?
         self.title = title #or should this be self.name = name? 
+        self.body = body
         self.owner = owner
 
 
+#create a new class that can be managed by sqlalchemy & store users in a db
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
@@ -33,6 +35,7 @@ class User(db.Model):
         self.password = password
 
 
+#write a function that requires login before accessing the blog & newpost pages
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'register']
@@ -40,6 +43,7 @@ def require_login():
         return redirect('/login')
 
 
+#write a function that allows a user to login & go to their blog page - if not send them back to login
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -49,13 +53,14 @@ def login():
         if user and user.password == password:
             session['email'] = email
             flash('Logged in')
-            return redirect('/')
+            return redirect('/blog')
         else:
             flash('User password incorrect, or user does not exist', 'error')
 
-        return render_template('login.html')
+    return render_template('login.html')
 
 
+#write a function that allows a user to register as a user
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
@@ -82,17 +87,47 @@ def logout():
     return redirect('/')
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+@app.route('/blog') #this route should list blogs via get method
+def list_blogs():
 
+    owner = User.query.filter_by(email=session['email']).first()
+    blogs = Blog.query.filter_by(owner=owner).all()
+    blog_title = request.form['title']
+
+    return redirect('/blog')
+
+
+@app.route('/newpost', methods=['POST', 'GET']) #this route will let you post a blog
+def post_blog():
+     
     owner = User.query.filter_by(email=session['email']).first()
 
     if request.method == 'POST':
-        blog_title = request.form['blog'] #check again if this should say 'name' instead of 'title'
-        new_blog = Blog(blog_title, owner)
+        blog_title = request.form['title'] #check again if this should say 'name' instead of 'title'
+        blog_body = request.form['body']
+        new_blog = Blog(blog_title, blog_body, owner)
         db.session.add(new_blog)
         db.session.commit()
 
     blogs = Blog.query.filter_by(owner=owner).all()
 
+    return render_template('newpost.html')
 
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+
+    owner = User.query.filter_by(email=session['email']).first() 
+    
+    if request.method == 'POST':   #if request is a post (if it came from submitting the form)
+        blog_name = request.form['blog']   #then we want to grab the data out of the user's input 
+        new_blog = Blog(blog_name, owner)  #create new Blog object 
+        db.session.add(new_blog) #add it to future commits
+        db.session.commit()   #commit to database as a persistent object
+
+    blogs = Blog.query.filter_by(owner=owner).all() #owner=owner specifies that this blog belongs to this user(owner)
+    return render_template('blog.html', title='Get writing!', blogs=blogs)
+
+
+if __name__ == '__main__':
+    app.run()
